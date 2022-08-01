d5b0c1483e3870c58ef8404b41e3453eec2548b3
kevinlinpt <kevinlinpt+1@hotmail.com>
2022-08-01 18:12:54 -0400

    First Commit

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,94 +1,162 @@
+// create shows section element and add classes
 var showsContainer = document.createElement("section");
 showsContainer.classList.add("main-section");
+showsContainer.classList.add("main-section__shows");
+
+// create heading 'shows' and add classes for styling; append into show section
 var showsHeading = document.createElement("h2");
 showsHeading.classList.add(
   "section-header",
   "section-header__padding",
   "section-header--black"
 );
+
 var headingContent = document.createTextNode("Shows");
 showsHeading.appendChild(headingContent);
 showsContainer.appendChild(showsHeading);
 document.body.appendChild(showsContainer);
 
-var showdata = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-    button: "<button class='btn'>BUY TICKETS</button>",
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-    button: "<button class='btn'>BUY TICKETS</button>",
-  },
-
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-    button: "<button class='btn'>BUY TICKETS</button>",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-    button: "<button class='btn'>BUY TICKETS</button>",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-    button: "<button class='btn'>BUY TICKETS</button>",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-    button: "<button class='btn'>BUY TICKETS</button>",
-  },
-];
-function generate_shows(data) {
-  // creates a <table> element
-  const tbl = document.createElement("table");
-  tbl.classList = "shows";
+// function to generate and display above array of objects
+function generate_shows(result) {
+  // creates a container for the show headers and data
+  const showTable = document.createElement("div");
+  showTable.classList = "shows";
   var btn = document.createElement("input");
-  //  const tblHeader = document.createElement("th")
-  //  tblHeaderCell = document.createTextNode("Full Name")
+  const showsContent = document.createElement("div");
 
   // creating all cells
-  for (let i = 0; i < data.length; i++) {
-    // creates a table row
-    var show = data[i];
-    const row = document.createElement("tr");
-    var properties = ["date", "venue", "location", "button"];
+  for (let i = 0; i < result.data.length; i++) {
+    var show = result.data[i];
+    show.headerDate = "DATE";
+    show.headerVenue = "VENUE";
+    show.headerLocation = "LOCATION";
+    show.button = "BUY TICKETS";
+    console.log(show);
+    // container for each row
+    const showInfoContainer = document.createElement("div");
+    showInfoContainer.classList = "show-info-container";
 
-    for (let j = 0; j < properties.length; j++) {
-      const cell = document.createElement("td");
-      cell.innerHTML = show[properties[j]];
-      row.appendChild(cell);
-      cell.classList.add("body-copy");
-    }
+    // show date, date header & date container
+    const cellHeaderDate = document.createElement("div");
+    cellHeaderDate.innerText = show.headerDate;
+    cellHeaderDate.classList = "show-header-date";
+    cellHeaderDate.classList.add("body-copy");
 
-    // add the row to the end of the table body
-    tbl.appendChild(row);
+    const cellDate = document.createElement("div");
+    cellDate.innerText = new Date(+show.date).toDateString(); // converts time in miliseconds (string) to number then to UTC, then human readable format.
+    cellDate.classList = "show-date";
+    cellDate.classList.add("body-copy");
+
+    const cellDateContainer = document.createElement("div");
+    cellDateContainer.classList = "show-date-container";
+    cellDateContainer.appendChild(cellHeaderDate);
+    cellDateContainer.appendChild(cellDate);
+    showInfoContainer.appendChild(cellDateContainer);
+    showsContent.appendChild(showInfoContainer);
+
+    // show venue, venue header & venue container
+    const cellHeaderVenue = document.createElement("div");
+    cellHeaderVenue.innerText = show.headerVenue;
+    showsContent.appendChild(cellHeaderVenue);
+    cellHeaderVenue.classList = "show-header-venue";
+    cellHeaderVenue.classList.add("body-copy");
+
+    const cellVenue = document.createElement("div");
+    cellVenue.innerText = show.place;
+    showsContent.appendChild(cellVenue);
+    cellVenue.classList = "show-venue";
+    cellVenue.classList.add("body-copy");
+
+    const cellVenueContainer = document.createElement("div");
+    cellVenueContainer.classList = "show-venue-container";
+    cellVenueContainer.appendChild(cellHeaderVenue);
+    cellVenueContainer.appendChild(cellVenue);
+    showInfoContainer.appendChild(cellVenueContainer);
+    showsContent.appendChild(showInfoContainer);
+
+    // show location & location header
+    const cellHeaderLocation = document.createElement("div");
+    cellHeaderLocation.innerText = show.headerLocation;
+    showsContent.appendChild(cellHeaderLocation);
+    cellHeaderLocation.classList = "show-header-location";
+    cellHeaderLocation.classList.add("body-copy");
+
+    const cellLocation = document.createElement("div");
+    cellLocation.innerText = show.location;
+    showsContent.appendChild(cellLocation);
+    cellLocation.classList = "show-location";
+    cellLocation.classList.add("body-copy");
+
+    const cellLocationContainer = document.createElement("div");
+    cellLocationContainer.classList = "show-location-container";
+    cellLocationContainer.appendChild(cellHeaderLocation);
+    cellLocationContainer.appendChild(cellLocation);
+    showInfoContainer.appendChild(cellLocationContainer);
+    showsContent.appendChild(showInfoContainer);
+
+    // show button - buy tickets
+    const cellButton = document.createElement("button");
+    cellButton.innerText = show.button;
+    showInfoContainer.appendChild(cellButton);
+    showsContent.appendChild(showInfoContainer);
+    cellButton.classList = "show-button-tickets";
+    cellButton.classList.add("body-copy");
+
+    // divider
+    const divider = document.createElement("hr");
+    divider.classList = "divider";
+    showsContent.appendChild(divider);
   }
-  // appends <table> into <body>
-  document.body.appendChild(tbl);
+  // table header container
+  const headerContainer = document.createElement("div");
+  headerContainer.classList = "header-container";
+
+  const headerDate = document.createElement("div");
+  headerDate.innerText = show.headerDate;
+  headerDate.classList = "header-date";
+  headerDate.classList.add("body-copy");
+
+  const headerDateContainer = document.createElement("div");
+  headerDateContainer.classList = "show-date-container";
+  headerDateContainer.appendChild(headerDate);
+  headerContainer.appendChild(headerDateContainer);
+
+  const headerVenue = document.createElement("div");
+  headerVenue.innerText = show.headerVenue;
+  headerVenue.classList = "header-venue";
+  headerVenue.classList.add("body-copy");
+
+  const headerVenueContainer = document.createElement("div");
+  headerVenueContainer.classList = "show-venue-container";
+  headerVenueContainer.appendChild(headerVenue);
+  headerContainer.appendChild(headerVenueContainer);
+
+  const headerLocation = document.createElement("div");
+  headerLocation.innerText = show.headerLocation;
+  headerLocation.classList = "header-location";
+  headerLocation.classList.add("body-copy");
+
+  const headerLocationContainer = document.createElement("div");
+  headerLocationContainer.classList = "header-location-container";
+  headerLocationContainer.appendChild(headerLocation);
+  headerContainer.appendChild(headerLocationContainer);
+
+  // appends the table of show data into <body> before the footer
+  showTable.appendChild(headerContainer);
+  showTable.appendChild(showsContent);
+  showsContainer.appendChild(showTable);
+  document.body.insertBefore(
+    showsContainer,
+    document.getElementsByClassName("footer")
+  );
+  // document.body.appendChild(showsContainer);
 }
 
-// tbl.classList.add("shows-table");
-// tblBody.classList.add("shows-body");
-// row.classList.add("shows-row");
-// cell.classList.add("shows-cell");
-// cellText.classList.add("shows-text");
-
-generate_shows(showdata);
-var tblContainer = document.createElement("div");
-tblContainer.classList.add("tbl-container");
-table = document.querySelector(".shows");
-tblContainer.append(table);
-showsContainer.appendChild(tblContainer);
-document.body.appendChild(showsContainer);
+// gets API from link and calls generate_shows function using the JSON provided
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/showdates?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4"
+  )
+  .then((result) => {
+    generate_shows(result);
+  });
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,81 +1,110 @@
-var post = document.querySelector(".comment");
+// Eventlistener for comment button
+var post = document.querySelector(".comment"); // button class name
 post.addEventListener("click", function () {
-  var commentBoxValue = document.querySelector(".comment-box__name").value;
-  var commentBoxValueComment = document.querySelector(
+  const commentBoxValueName =
+    document.querySelector(".comment-box__name").value;
+  commentBoxValueName.value;
+  const commentBoxValueBody = document.querySelector(
     ".comment-box__comment"
   ).value;
-  var addObj = {
-    name: commentBoxValue,
-    date: "07-17-2022",
-    body: commentBoxValueComment,
-  };
 
+  // retrieves current date of user's time of post and outputs as a string
+  const date = new Date();
+  currentDate =
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    "/" +
+    ("0" + date.getDate()).slice(-2) +
+    "/" +
+    date.getFullYear();
+
+  // new comment object to be inserted into function displayComments
+  var addObj = {
+    // create array name of data to match API
+    data: [
+      {
+        name: commentBoxValueName,
+        timestamp: currentDate,
+        comment: commentBoxValueBody,
+      },
+    ],
+  };
   displayComment(addObj);
-  console.log(commentBoxValue);
-  commentBoxValue = "";
-  commentBoxValueComment = "";
-  // var p = document.createElement("p");
-  // var text = document.createTextNode(commentBoxValue);
-  // p.appendChild(text);
-  // document.querySelector(".description-comment").appendChild(p);
+
+  document.querySelector(".comment-box__name").value = "";
+  document.querySelector(".comment-box__comment").value = "";
 });
 
-var existingComments = [
-  {
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    body: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-  {
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    body: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
+// displays comments to the user
+function displayComment(result) {
+  var resultReverse = result.data.reverse();
+  for (var i = 0; i < result.data.length; i++) {
+    // create all parent/child elements for comment box
+    const userName = document.createElement("div");
+    const userDate = document.createElement("div");
+    const userBody = document.createElement("div");
+    const leftPanelExisting = document.createElement("div"); // container for the avatar
+    const avatar = document.createElement("img"); // leave it blank and set background color to silver
+    const rightPanelExisting = document.createElement("div"); // container for user container and comment description
+    const userContainer = document.createElement("div"); // container for user information
+    const commentContainer = document.createElement("div");
 
-  {
-    name: "Connor Walton",
-    date: "02/17/2021",
-    body: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-];
+    // give each element a class name
+    userName.classList = "username";
+    userDate.classList = "userdate";
+    userBody.classList = "userbody";
+    leftPanelExisting.classList = "left-panel__existing";
+    avatar.classList = "avatar";
+    rightPanelExisting.classList = "right-panel__existing";
+    userContainer.classList = "user-container";
+    commentContainer.classList = "comment-container";
 
-// const username = document.createElement("span");
-// const userdate = document.createElement("div");
-// const userbody = document.createElement("div");
+    // set avatar to placeholder image
+    avatar.src = "https://via.placeholder.com/150";
 
-function displayComment(data) {
-  var commentContent =
-    "<hr/><div class= 'left-panel__existing'><img src= 'https://place-hold.it/300'/></div><div class= 'right-panel__existing'><div class= 'user-container'<div class= <span>" +
-    data.name +
-    "</span><div class= 'date'>" +
-    data.date +
-    "</div></div><p class='existing-description'>" +
-    data.body +
-    "</p></div>";
+    // append children into left panel
+    leftPanelExisting.appendChild(avatar);
 
-  var commentContainer = document.createElement("div");
-  // console.log(commentContent);
-  commentContainer.innerHTML = commentContent;
-  commentContainer.classList.add("comment-container");
-  if (document.querySelector(".comment-container")) {
-    document
-      .querySelector(".existing-comments")
-      .insertBefore(
+    // append children into right panel
+    userName.innerText = resultReverse[i].name;
+    const datePosted = new Date(resultReverse[i].timestamp);
+    dateFormat =
+      ("0" + (datePosted.getMonth() + 1)).slice(-2) +
+      "/" +
+      ("0" + datePosted.getDate()).slice(-2) +
+      "/" +
+      datePosted.getFullYear();
+    userDate.innerText = dateFormat;
+    userBody.innerText = resultReverse[i].comment;
+
+    userContainer.appendChild(userName);
+    userContainer.appendChild(userDate);
+    rightPanelExisting.appendChild(userContainer);
+    rightPanelExisting.appendChild(userBody);
+
+    // append left and right panel into existing-comments container
+    commentContainer.appendChild(leftPanelExisting);
+    commentContainer.appendChild(rightPanelExisting);
+    const existingCommentContainer =
+      document.querySelector(".existing-comments");
+    existingCommentContainer.appendChild(commentContainer);
+
+    // inserts posted comments to the top of comment section using insertBefore
+    if (commentContainer) {
+      existingCommentContainer.insertBefore(
         commentContainer,
-        document.querySelector(".existing-comments").firstChild
+        existingCommentContainer.firstChild
       );
-  } else {
-    document.querySelector(".existing-comments").appendChild(commentContainer);
+    } else {
+      existingCommentContainer.appendChild(commentContainer);
+    }
   }
 }
 
-for (var i = 0; i < existingComments.length; i++) {
-  displayComment(existingComments[i]);
-}
-
-// document.queryselector(".existing-description").innerHTML =
-//   existingComments.name + "";
-
-// var commentsExisting = document.querySelector(".existing-description");
-// commentsExisting.textContent += existingComments;
-// for (i = 0; i < existingComments.length; i++) {}
+// gets API from link and calls displayComment function using the JSON provided
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/comments?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4"
+  )
+  .then((result) => {
+    displayComment(result);
+  });
